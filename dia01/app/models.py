c60675cddc8e52fde420fbc101bc29f07cb4b951
gabrielcunha10 <gabrielcunha066@gmail.com>
2025-09-09 16:50:15 -0300

    app

--- dia01/app/models.py
+++ dia01/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.CharField()
+    email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return self.nome
