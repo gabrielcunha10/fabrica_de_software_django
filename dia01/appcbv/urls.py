9f00a439031ee23b0e886949e07232798bd6f93a
gabrielcunha10 <gabrielcunha066@gmail.com>
2025-09-11 17:36:37 -0300

    lista de cachorros

--- dia01/appcbv/urls.py
+++ dia01/appcbv/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HelloView
+from .views import HelloView, CachorroListView
 
 urlpatterns = [
-    path('',HelloView.as_view(), name="index")
+    path('',HelloView.as_view(), name="index"),
+    path("listacachorro/", CachorroListView.as_view(), name='listacachorro')
 ]
--- dia01/appcbv/views.py
+++ dia01/appcbv/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
 from django.http import HttpResponse
-
+from .models import Cachorro
 class HelloView(View):
     def get(self, request):
         return HttpResponse("Boa tarde.")
+class CachorroListView(ListView):
+    model = Cachorro
+    template_name = "lista.html"
+    context_object_name = "cachorros"
