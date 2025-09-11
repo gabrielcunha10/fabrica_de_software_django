08d7ca43c5ea35e140615ecd44c9836a3cccca00
gabrielcunha10 <gabrielcunha066@gmail.com>
2025-09-11 16:32:09 -0300

    atualizar usuario e deletar usuario

--- dia01/app/urls.py
+++ dia01/app/urls.py
@@ -5,6 +5,8 @@ from .views import criar_pessoa
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('lista/', listar_pessoas, name='listar_pessoas'),
-    path('criar/', criar_pessoa, name='criar_pessoa'),
+    path('lista', listar_pessoas, name='listar_pessoas'),
+    path('criar', criar_pessoa, name='criar_pessoa'),
+    path('deletar/<int:pk>', views.deletar_pessoa, name='deletar_pessoa'),
+    path('atualizar/<int:pk>', views.atualizar_pessoa, name='atualizar_pessoa'),
 ]
--- dia01/app/views.py
+++ dia01/app/views.py
@@ -20,3 +20,21 @@ def criar_pessoa(request):
     else:
         form = PessoaForm()
     return render(request, 'create.html', {'pessoa': form})
+
+def deletar_pessoa(request, pk):
+    pessoa = Usuario.objects.get(pk=pk)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('listar_pessoas')
+    return render(request, 'confirmar_delete.html',{'pessoa': pessoa})
+
+def atualizar_pessoa(request, pk):
+    pessoa = Usuario.objects.get(pk=pk)
+    if request.method == "POST":
+        form = PessoaForm(request.POST, instance=pessoa)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_pessoas')
+    else:
+        form = PessoaForm(instance=pessoa)
+    return render(request, 'create.html', {'pessoa': form})
