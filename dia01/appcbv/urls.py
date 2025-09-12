9f00a67133b847571119805fd3bfda28c2b9e974
gabrielcunha10 <gabrielcunha066@gmail.com>
2025-09-12 17:01:37 -0300

    criar e deletar cachorro

--- dia01/appcbv/urls.py
+++ dia01/appcbv/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import HelloView, CachorroListView
+from .views import HelloView, CachorroListView, CachorroCreateView, CachorroDeleteView, CachorroUpdateView
 
 urlpatterns = [
     path('',HelloView.as_view(), name="index"),
-    path("listacachorro/", CachorroListView.as_view(), name='listacachorro')
+    path("listacachorro/", CachorroListView.as_view(), name='listacachorro'),
+    path("criar/", CachorroCreateView.as_view(), name = "criarcachorro"),
+    path("atualizar/<int:pk>", CachorroUpdateView.as_view(), name = "atualizarcachorro"),
+    path("deletar/<int:pk>", CachorroDeleteView.as_view(), name = "deletarcachorro"),
 ]
--- dia01/appcbv/views.py
+++ dia01/appcbv/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from .models import Cachorro
+from .forms import CachorroForm
+
+
 class HelloView(View):
     def get(self, request):
         return HttpResponse("Boa tarde.")
@@ -10,3 +14,22 @@ class CachorroListView(ListView):
     model = Cachorro
     template_name = "lista.html"
     context_object_name = "cachorros"
+
+class CachorroCreateView(CreateView):
+    model = Cachorro
+    form_class = CachorroForm
+    template_name = "criarcachorro.html"
+    success_url = reverse_lazy("listacachorro")
+
+class CachorroUpdateView(UpdateView):
+    model = Cachorro
+    form_class = CachorroForm
+    template_name = "criarcachorro.html"
+    context_object_name = "atualizacachorro"
+    success_url = reverse_lazy("listacachorro")
+
+class CachorroDeleteView(DeleteView):
+    model = Cachorro
+    template_name = "deletarcachorro.html"
+    context_object_name = "cachorro"
+    success_url = reverse_lazy("listacachorro")
